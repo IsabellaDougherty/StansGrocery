182f7ccce92e981e1e03b486bb9a01b4bb4d3218
Isabella <belladougherty@gmail.com>
2023-04-25 10:31:53 -0600

    Added comments for the current code (subject to updates/changes)

--- StansGrocery/StansGroceryForm.vb
+++ StansGrocery/StansGroceryForm.vb
@@ -31,6 +31,9 @@ Public Class StansGroceryForm
     Dim item As String
     Dim values As String()
     Dim currentLength As Integer
+
+
+    'Loading the form and adding information to combo box
     Private Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
 
@@ -69,6 +72,7 @@ Public Class StansGroceryForm
         FilterByAisleRadioButton.Checked = True
     End Sub
 
+    'Add items to display box
     Private Sub AddItems(item As String)
         If Not items.Contains(item) Then
             items.Add(item)
@@ -76,6 +80,7 @@ Public Class StansGroceryForm
         End If
     End Sub
 
+    'If the aisle number isn't already added will add it
     Private Sub AisleFilterAddIfChecked(aisle As String)
         categories.Clear()
         If FilterByAisleRadioButton.Checked = True Then
@@ -85,12 +90,14 @@ Public Class StansGroceryForm
         End If
     End Sub
 
+    'Adds the aisle numbers that have been filtered and sorted to the combo box
     Private Sub AisleFilterAdd()
         For i = 0 To aisleNumbers.Count - 1
             FilterComboBox.Items.Add(aisleNumbers(i))
         Next
     End Sub
 
+    'Sorts the aisle numbers so they are in order numerically rather then in order of how they were added from the file
     Private Sub SortAisles(aisle As String)
         Try
             aisleNumbers.Add(CInt(aisle))
@@ -106,6 +113,7 @@ Public Class StansGroceryForm
         End Try
     End Sub
 
+    'Adds the catagories to the combo box so long as that catagory hasn't already been added before
     Private Sub CatagoryFilterAdd(category As String)
         aisleNumbers.Clear()
         If FilterByCatagoryRadioButton.Checked = True Then
@@ -118,11 +126,13 @@ Public Class StansGroceryForm
         End If
     End Sub
 
+    'Displays about form
     Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutMenuItem.Click
         Me.Hide()
         AboutForm.Show()
     End Sub
 
+    'Closes all forms of the program
     Private Sub ExitMenuItem_Click_1(sender As Object, e As EventArgs) Handles ExitMenuItem.Click
         Dim yesNo As Integer
         yesNo = MsgBox("Are you sure you would like to quit?", CType(vbQuestion + vbYesNo + vbDefaultButton2, MsgBoxStyle), "Exit Menu")
@@ -134,6 +144,7 @@ Public Class StansGroceryForm
         End If
     End Sub
 
+    'Makes it so only aisle numbers are displayed in the combo box
     Private Sub FilterByAisleRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles FilterByAisleRadioButton.CheckedChanged
 
         If FilterByAisleRadioButton.Checked = True Then
@@ -150,6 +161,7 @@ Public Class StansGroceryForm
         End If
     End Sub
 
+    'Makes it so only catagories are displayed in the combo box
     Private Sub FilterByCatagoryRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles FilterByCatagoryRadioButton.CheckedChanged
 
         If FilterByCatagoryRadioButton.Checked = True Then
