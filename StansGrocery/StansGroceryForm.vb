dd0295758902f6fc99a0f7b746342ce8bdc99546
Isabella <belladougherty@gmail.com>
2023-05-04 17:43:09 -0600

    Finally got filtering to work

--- StansGrocery/StansGroceryForm.vb
+++ StansGrocery/StansGroceryForm.vb
@@ -81,6 +81,158 @@ Public Class StansGroceryForm
             End If
         Next
         ShowAllRadioButton.Checked = True
+        DisplayListBox.Sorted = True
+    End Sub
+
+    'Takes the in put of the search text button and pulls up all items available from the 
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        If SearchTextBox.Text = "zzz" Then
+            Dim yesNo As Integer
+            yesNo = MsgBox("Are you sure you would like to quit?", CType(vbQuestion + vbYesNo + vbDefaultButton2, MsgBoxStyle), "Exit Menu")
+            If yesNo = vbYes Then
+                Me.Close()
+                SplashScreenForm.Close()
+                AboutForm.Close()
+                End
+            End If
+        Else
+            DisplayListBox.Items.Clear()
+            If products IsNot Nothing AndAlso products.Length > 0 Then
+                For Each product In products
+                    If product.IndexOf(SearchTextBox.Text, StringComparison.CurrentCultureIgnoreCase) >= 0 Then
+                        DisplayListBox.Items.Add(product)
+                    End If
+                Next
+                If DisplayListBox.Items.Count = 0 Then
+                    MsgBox("No matching items found.")
+                End If
+                DisplayListBox.Refresh()
+            End If
+        End If
+        FilterItems()
+    End Sub
+
+    'Displays about form
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutMenuItem.Click
+        Me.Hide()
+        AboutForm.Show()
+    End Sub
+
+    'Closes all forms of the program
+    Private Sub ExitMenuItem_Click_1(sender As Object, e As EventArgs) Handles ExitMenuItem.Click
+        Dim yesNo As Integer
+        yesNo = MsgBox("Are you sure you would like to quit?", CType(vbQuestion + vbYesNo + vbDefaultButton2, MsgBoxStyle), "Exit Menu")
+        If yesNo = vbYes Then
+            Me.Close()
+            SplashScreenForm.Close()
+            AboutForm.Close()
+            End
+        End If
+    End Sub
+
+    Private Sub ShowAllRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles ShowAllRadioButton.CheckedChanged
+        FilterComboBox.Items.Clear()
+        categories.Clear()
+        aisleNumbers.Clear()
+        For Each line As String In lines
+            values = line.Split(",")
+            If (values(2).Replace("%%CAT", "").Replace("""", "").Trim()).Length = 0 Then
+            Else
+                category = values(2).Replace("%%CAT", "").Replace("""", "").Trim() 'get the category, remove any unnecessary characters and trim whitespace
+                CatagoryFilterAdd(category)
+            End If
+        Next
+        FilterComboBox.Sorted = True
+        FilterComboBox.Sorted = False
+        For Each line As String In lines
+            values = line.Split(",")
+            If (values(1).Replace("##LOC", "").Replace("""", "").Trim()).Length = 0 Then
+            Else
+                aisle = values(1).Replace("##LOC", "").Replace("""", "").Trim() 'get the aisle number, remove any unnecessary characters and trim whitespace
+                AisleFilterAddIfChecked(aisle)
+            End If
+        Next
+        AisleFilterAdd()
+    End Sub
+
+    'Makes it so only aisle numbers are displayed in the combo box
+    Private Sub FilterByAisleRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles FilterByAisleRadioButton.CheckedChanged
+
+        If FilterByAisleRadioButton.Checked = True Then
+            FilterComboBox.Sorted = False
+            FilterComboBox.Items.Clear()
+            categories.Clear()
+            For Each line As String In lines
+                values = line.Split(",")
+                If (values(1).Replace("##LOC", "").Replace("""", "").Trim()).Length = 0 Then
+                Else
+                    aisle = values(1).Replace("##LOC", "").Replace("""", "").Trim() 'get the aisle number, remove any unnecessary characters and trim whitespace
+                    AisleFilterAddIfChecked(aisle)
+                End If
+            Next
+            AisleFilterAdd()
+        End If
+    End Sub
+
+    'Makes it so only catagories are displayed in the combo box
+    Private Sub FilterByCatagoryRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles FilterByCatagoryRadioButton.CheckedChanged
+        If FilterByCatagoryRadioButton.Checked = True Then
+            aisleNumbers.Clear()
+            categories.Clear()
+            FilterComboBox.Items.Clear()
+            For Each line As String In lines
+                values = line.Split(",")
+                If (values(2).Replace("%%CAT", "").Replace("""", "").Trim()).Length = 0 Then
+                Else
+                    category = values(2).Replace("%%CAT", "").Replace("""", "").Trim() 'get the category, remove any unnecessary characters and trim whitespace
+                    CatagoryFilterAdd(category)
+                End If
+            Next
+            FilterComboBox.Sorted = True
+        End If
+
+    End Sub
+
+
+    'Filters items being displayed in the list box
+    Private Sub FilterItems()
+        Dim filterName As String = CStr(FilterComboBox.SelectedItem)
+        Dim isAisle As Boolean = False
+        Dim isCategory As Boolean = False
+        If filterName IsNot Nothing Then
+            Try
+                tempNum = CInt(filterName)
+                isAisle = True
+                tempNum = Nothing
+            Catch ex As Exception
+                isCategory = True
+            End Try
+        End If
+
+        Dim itemsToRemove As New List(Of Object)
+        For i = DisplayListBox.Items.Count - 1 To 0 Step -1
+            Dim product As String = DisplayListBox.Items(i).ToString()
+            For j = 0 To catalog.GetLength(1) - 1
+                If isAisle AndAlso filterName <> catalog(1, j) Then
+                    If product = catalog(0, j) Then
+                        itemsToRemove.Add(DisplayListBox.Items(i))
+                    End If
+                End If
+                If isCategory AndAlso filterName <> catalog(2, j) Then
+                    If product = catalog(0, j) Then
+                        itemsToRemove.Add(DisplayListBox.Items(i))
+                    End If
+                End If
+            Next
+        Next
+        For Each itemToRemove In itemsToRemove
+            DisplayListBox.Items.Remove(itemToRemove)
+        Next
+
+        If DisplayListBox.Items.Count = 0 Then
+            MsgBox("No items available that match your search")
+        End If
+
     End Sub
 
     'Creates a 2D array that holds all the items, aisles, and catagories that are given in the file.
@@ -97,27 +249,6 @@ Public Class StansGroceryForm
         catalog(1, countTwo) = values(1).Replace("##LOC", "").Replace("""", "").Trim()
         catalog(2, countTwo) = values(2).Replace("%%CAT", "").Replace("""", "").Trim()
         countTwo += 1
-    End Sub
-
-    'Takes the in put of the search text button and pulls up all items available from the 
-    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
-        If SearchTextBox.Text = "" Then
-            MsgBox("Please enter your searching querry.")
-        Else
-            DisplayListBox.Items.Clear()
-            If products IsNot Nothing AndAlso products.Length > 0 Then
-                For Each product In products
-                    If product.IndexOf(SearchTextBox.Text, StringComparison.CurrentCultureIgnoreCase) >= 0 Then
-                        DisplayListBox.Items.Add(product)
-                    End If
-                Next
-                If DisplayListBox.Items.Count = 0 Then
-                    MsgBox("No matching items found.")
-                End If
-                DisplayListBox.Refresh() ' Refresh the list box to display the updated list
-            End If
-        End If
-        DisplayListBox.Sorted = True
     End Sub
 
     'Add items to display box
@@ -170,84 +301,6 @@ Public Class StansGroceryForm
         End If
     End Sub
 
-    'Displays about form
-    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutMenuItem.Click
-        Me.Hide()
-        AboutForm.Show()
-    End Sub
-
-    'Closes all forms of the program
-    Private Sub ExitMenuItem_Click_1(sender As Object, e As EventArgs) Handles ExitMenuItem.Click
-        Dim yesNo As Integer
-        yesNo = MsgBox("Are you sure you would like to quit?", CType(vbQuestion + vbYesNo + vbDefaultButton2, MsgBoxStyle), "Exit Menu")
-        If yesNo = vbYes Then
-            Me.Close()
-            SplashScreenForm.Close()
-            AboutForm.Close()
-            End
-        End If
-    End Sub
-
-    Private Sub ShowAllRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles ShowAllRadioButton.CheckedChanged
-        FilterComboBox.Items.Clear()
-        categories.Clear()
-        aisleNumbers.Clear()
-        For Each line As String In lines
-            values = line.Split(",")
-            If (values(2).Replace("%%CAT", "").Replace("""", "").Trim()).Length = 0 Then
-            Else
-                category = values(2).Replace("%%CAT", "").Replace("""", "").Trim() 'get the category, remove any unnecessary characters and trim whitespace
-                CatagoryFilterAdd(category)
-            End If
-        Next
-        FilterComboBox.Sorted = True
-        FilterComboBox.Sorted = False
-        For Each line As String In lines
-            values = line.Split(",")
-            If (values(1).Replace("##LOC", "").Replace("""", "").Trim()).Length = 0 Then
-            Else
-                aisle = values(1).Replace("##LOC", "").Replace("""", "").Trim() 'get the aisle number, remove any unnecessary characters and trim whitespace
-                AisleFilterAddIfChecked(aisle)
-            End If
-        Next
-        AisleFilterAdd()
-    End Sub
-
-    'Makes it so only aisle numbers are displayed in the combo box
-    Private Sub FilterByAisleRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles FilterByAisleRadioButton.CheckedChanged
-
-        If FilterByAisleRadioButton.Checked = True Then
-            FilterComboBox.Items.Clear()
-            categories.Clear()
-            For Each line As String In lines
-                values = line.Split(",")
-                If (values(1).Replace("##LOC", "").Replace("""", "").Trim()).Length = 0 Then
-                Else
-                    aisle = values(1).Replace("##LOC", "").Replace("""", "").Trim() 'get the aisle number, remove any unnecessary characters and trim whitespace
-                    AisleFilterAddIfChecked(aisle)
-                End If
-            Next
-            AisleFilterAdd()
-        End If
-    End Sub
-
-    'Makes it so only catagories are displayed in the combo box
-    Private Sub FilterByCatagoryRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles FilterByCatagoryRadioButton.CheckedChanged
-        aisleNumbers.Clear()
-        If FilterByCatagoryRadioButton.Checked = True Then
-            FilterComboBox.Items.Clear()
-            For Each line As String In lines
-                values = line.Split(",")
-                If (values(2).Replace("%%CAT", "").Replace("""", "").Trim()).Length = 0 Then
-                Else
-                    category = values(2).Replace("%%CAT", "").Replace("""", "").Trim() 'get the category, remove any unnecessary characters and trim whitespace
-                    CatagoryFilterAdd(category)
-                End If
-            Next
-            FilterComboBox.Sorted = True
-        End If
-
-    End Sub
 
 
     Private Sub ItemList()
